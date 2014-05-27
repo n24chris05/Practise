33488be60b11a51355b3224ac93c5cba6781c85a
n24chris05 <ChrisiMac@Chriss-iMac-5.local>
2014-05-27 18:58:44 +0800

    update

--- family/family/Namelist.h
+++ family/family/Namelist.h
@@ -4,11 +4,14 @@
 //
 //  Created by Chris Arquelada on 5/27/14.
 //  Copyright (c) 2014 Chris Arquelada. All rights reserved.
-//
+// Namelist is a singleton
 
 #import <Foundation/Foundation.h>
 
 @interface Namelist : NSObject
-@property (nonatomic,readonly)NSArray *allNames;    
+
+//Notice that this is a class method and prefixed with a + instead of -
++(instancetype) member;
+
 
 @end
--- family/family/Namelist.m
+++ family/family/Namelist.m
@@ -10,6 +10,28 @@
 
 @implementation Namelist
 
++(instancetype)member
+{
+    static Namelist *member = nil;
+    
+    //Do I need to create member?
+    if (!member){
+        member = [[self alloc]initPrivate];
+    }
+    
+    return member;
+}
 
+//secret is the real (secret)initializer
+-(instancetype)initPrivate
+{
+    self = [super init];
+    return self;
+}
 
+-(instancetype)init
+{
+    @throw [NSException exceptionWithName:@"Singleton"
+                                   reason:@"User + [nameList member]" userInfo:nil];
+}
 @end

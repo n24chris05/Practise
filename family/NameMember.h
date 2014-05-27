eb71285597654c1f8c2bc5028688bac68ddec119
n24chris05 <ChrisiMac@Chriss-iMac-5.local>
2014-05-27 20:00:58 +0800

    1
    1

---
+++ family/NameMember.h
@@ -0,0 +1,13 @@
+//
+//  NameMember.h
+//  family
+//
+//  Created by Chris Arquelada on 5/27/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NameMember : NSObject
+
+@end
---
+++ family/NameMember.m
@@ -0,0 +1,13 @@
+//
+//  NameMember.m
+//  family
+//
+//  Created by Chris Arquelada on 5/27/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import "NameMember.h"
+
+@implementation NameMember
+
+@end

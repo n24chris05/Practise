ccf327411b8e2f614e4df82e061beba0af4981aa
n24chris05 <ChrisMacBookPro@Chriss-MacBook-Pro.local>
2014-05-27 17:44:38 +0800

    upup

---
+++ family/family/Namelist.h
@@ -0,0 +1,14 @@
+//
+//  Namelist.h
+//  family
+//
+//  Created by Chris Arquelada on 5/27/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Namelist : NSObject
+@property (nonatomic,readonly)NSArray *allNames;    
+
+@end
---
+++ family/family/Namelist.m
@@ -0,0 +1,15 @@
+//
+//  Namelist.m
+//  family
+//
+//  Created by Chris Arquelada on 5/27/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import "Namelist.h"
+
+@implementation Namelist
+
+
+
+@end

1ef2e86ef0b7b1d84154803aee7e2b254328430f
n24chris05 <ChrisMacBookPro@Chriss-MacBook-Pro.local>
2014-05-29 17:40:21 +0800

    reHomepwner

---
+++ ReHomepwner/ReHomepwner/BNRItemViewController.h
@@ -0,0 +1,15 @@
+//
+//  BNRItemViewController.h
+//  ReHomepwner
+//
+//  Created by Chris Arquelada on 5/29/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemViewController : UITableViewController
+
+
+
+@end
---
+++ ReHomepwner/ReHomepwner/BNRItemViewController.m
@@ -0,0 +1,23 @@
+//
+//  BNRItemViewController.m
+//  ReHomepwner
+//
+//  Created by Chris Arquelada on 5/29/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import "BNRItemViewController.h"
+
+@implementation BNRItemViewController
+
+-(instancetype)init
+{
+    //Call the superclass designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+-(instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+@end

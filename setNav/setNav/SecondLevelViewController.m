46bae88563c9a2af7da653c8b4593a3941740f16
n24chris05 <ChrisiMac@Chriss-iMac-5.local>
2014-05-29 16:19:36 +0800

    231

---
+++ setNav/setNav/SecondLevelViewController.m
@@ -0,0 +1,18 @@
+//
+//  SecondLevelViewController.m
+//  setNav
+//
+//  Created by Chris Arquelada on 5/28/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import "SecondLevelViewController.h"
+
+@interface SecondLevelViewController ()
+
+@end
+
+@implementation SecondLevelViewController
+
+
+@end

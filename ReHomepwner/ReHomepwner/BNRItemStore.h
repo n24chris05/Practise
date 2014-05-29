f1403b5758e82176c90f419a7072585e1c86014d
n24chris05 <ChrisMacBookPro@Chriss-MacBook-Pro.local>
2014-05-29 20:09:51 +0800

    update

---
+++ ReHomepwner/ReHomepwner/BNRItemStore.h
@@ -0,0 +1,17 @@
+//
+//  BNRItemStore.h
+//  ReHomepwner
+//
+//  Created by Chris Arquelada on 5/29/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItemStore : NSObject
+
+//Notice that this is a class method
++(instancetype)sharedStore;
+
+
+@end
---
+++ ReHomepwner/ReHomepwner/BNRItemStore.m
@@ -0,0 +1,13 @@
+//
+//  BNRItemStore.m
+//  ReHomepwner
+//
+//  Created by Chris Arquelada on 5/29/14.
+//  Copyright (c) 2014 Chris Arquelada. All rights reserved.
+//
+
+#import "BNRItemStore.h"
+
+@implementation BNRItemStore
+
+@end

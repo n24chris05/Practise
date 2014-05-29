4b2f3d5f09770bf6336dd894f2257934cd5ad576
n24chris05 <ChrisMacBookPro@Chriss-MacBook-Pro.local>
2014-05-30 00:06:41 +0800

    bukol update

---
+++ ReHomePwner copy/HomePwner/BNRItemsViewController.m
@@ -0,0 +1,92 @@
+//
+//  BNRItemsViewController.m
+//  HomePwner
+//
+//  Created by John Gallagher on 1/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
+@implementation BNRItemsViewController
+
+- (instancetype)init
+{
+    // Call the superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    if (self) {
+        for (int i = 0; i < 5; i++) {
+            [[BNRItemStore sharedStore] createItem];
+
+        }
+    }
+    NSLog(@"sizeof(NSInteger) = %@", @(sizeof(NSInteger)));
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Get a new or recycled cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    
+    
+    
+    BNRItem *item = items[indexPath.row];
+    
+    if ([item valueInDollars]> 50){
+
+    }
+    NSLog(@"%d",[item valueInDollars]);
+    
+    
+    cell.textLabel.text = [item description];
+
+    return cell;
+}
+
+//section
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSString *sectionTitle = [[NSString alloc]init];
+    if (section==0){
+    
+        sectionTitle= @"Section 1";
+    }else if (section ==1){
+        sectionTitle= @"Section 2";
+    }
+    
+    return sectionTitle;
+}
+
+@end
